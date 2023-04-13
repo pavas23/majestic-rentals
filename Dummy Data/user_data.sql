bb55feb3ca1a315fa30f9076598581baa14924e5
Pavas Garg <pavasgarg@Pavass-MacBook-Pro.local>
2023-04-13 15:49:31 +0530

    added user dummy data

---
+++ Dummy Data/user_data.sql
@@ -0,0 +1,15 @@
+insert into USERS values('567891011234', 'Pavas Garg', 20, 'pavas@gmail.com', 'ashi@123', 'VK-101', 'Vishwakarma', 'Hyderabad', 'Telangana', '500078', 1, 0, 0, 0);
+insert into USERS values('842554382750', 'Vashisth Choudhari', 20, 'vashi@gmail.com', 'vasu@123', 'VK-102', 'Vishwakarma', 'Hyderabad', 'Telangana', '500078', 0, 1, 0, 0);
+insert into USERS values('446194813725', 'Dev Gala', 20, 'dev@gmail.com', 'gala@123', 'VK-103', 'Vishwakarma', 'Hyderabad', 'Telangana', '500078', 0, 1, 0, 0);
+insert into USERS values('302593322317', 'Kolasani Amit Vishnu', 20, 'amit@gmail.com', 'koala@123', 'VK-103', 'Vishwakarma', 'Hyderabad', 'Telangana', '500078', 0, 1, 0, 0);
+insert into USERS values('281211937995', 'Saksham Bajaj', 20, 'bajaj@gmail.com', 'saksh@123', 'VK-101', 'Vishwakarma', 'Hyderabad', 'Telangana', '500078', 0, 1, 0, 0);
+insert into USERS values('489791554284', 'Atharva Vinod Dashora', 35, 'atharva@gmail.com', 'dashu@123', 'VK-106', 'Vishwakarma', 'Hyderabad', 'Telangana', '500078', 0, 0, 1, 0);
+insert into USERS values('304216446808', 'Divyateja Pasupuleti', 40, 'divya@gmail.com', 'psps@123', 'VK-106', 'Vishwakarma', 'Hyderabad', 'Telangana', '500078', 0, 0, 1, 0);
+insert into USERS values('515481242429', 'Ravuri Nikhil Krishna', 47, 'nikhil@gmail.com', 'ravuri@123', 'VK-107', 'Vishwakarma', 'Hyderabad', 'Telangana', '500078', 0, 0, 1, 0);
+insert into USERS values('511488177684', 'Murari Sai Karthik', 33, 'karthik@gmail.com', 'murari@123', 'VK-104', 'Vishwakarma', 'Hyderabad', 'Telangana', '500078', 0, 0, 1, 0);
+insert into USERS values('971117121294', 'Gaurav Somai', 29, 'gaurav@gmail.com', 'somai@123', 'VK-104', 'Vishwakarma', 'Hyderabad', 'Telangana', '500078', 0, 0, 1, 0);
+insert into USERS values('801107112667', 'Harsh Rachalwar', 25, 'harsh@gmail.com', 'rachu@123', 'VK-112', 'Vishwakarma', 'Hyderabad', 'Telangana', '500078', 0, 0, 0, 1);
+insert into USERS values('674128999089', 'Manan Gupta', 27, 'manan@gmail.com', 'gupta@123', 'VK-111', 'Vishwakarma', 'Hyderabad', 'Telangana', '500078', 0, 0, 0, 1);
+insert into USERS values('643517034011', 'Rohan Pothireddy', 23, 'rohan@gmail.com', 'pothi@123', 'VK-106', 'Vishwakarma', 'Hyderabad', 'Telangana', '500078', 0, 0, 0, 1);
+insert into USERS values('228939267400', 'Shivam Atul Trivedi', 29, 'shivam@gmail.com', 'atul@123', 'VK-113', 'Vishwakarma', 'Hyderabad', 'Telangana', '500078', 0, 0, 0, 1);
+insert into USERS values('326744516683', 'Somananth Rotte', 30, 'somananth@gmail.com', 'rotte@123', 'VK-107', 'Vishwakarma', 'Hyderabad', 'Telangana', '500078', 0, 0, 0, 1);
